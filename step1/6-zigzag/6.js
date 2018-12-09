aee121373609de1223c4dacca27dbac8eaf5d3d8
songlairui <cnre@live.cn>
2018-12-09 23:55:25 +0800

    去了let，去了一个变量计算，就快了

--- step1/6-zigzag/6.js
+++ step1/6-zigzag/6.js
@@ -1,16 +1,15 @@
 module.exports = function convert(s, numRows = 0) {
-  if (numRows <= 1) return s;
+  if (numRows === 1) return s;
   let result = "";
   let length = s.length;
-  const chunk = numRows * 2 - 2;
-  for (let i = 0; i < numRows; i++) {
-    let j = i;
+  var chunk = numRows * 2 - 2;
+  for (var i = 0; i < numRows; i++) {
+    var j = i;
     const requireTwo = i !== 0 && i !== numRows - 1;
-    const diff = chunk - i - i;
     while (j < length) {
       result += s[j];
-      if (requireTwo && s[j + diff]) {
-        result += s[j + diff];
+      if (requireTwo && s[j + chunk - i - i]) {
+        result += s[j + chunk - i - i];
       }
       j += chunk;
     }
